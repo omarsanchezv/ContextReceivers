2899f5808deeb2f3cc38d0581cf1c7e849d07e1c
carlos.sanchez <carlos.sanchez@mlb.com>
2022-07-04 15:53:34 -0500

    context logger

--- src/main/kotlin/Logger.kt
+++ src/main/kotlin/Logger.kt
@@ -1,20 +1,20 @@
-class Logger(private val name: String) {
+class Logger(val name: String) {
     fun log(s: String) = println("$name: $s")
 }
 
-class NotificationSender{
+class NotificationSender(val name: String){
     fun send(s: String) = println("Notify: $s")
 }
 
 context (Logger, NotificationSender)
 fun store(s: String){
-    log("Stored $s on disk")
+    log("Stored $s on disk (from ${this@Logger.name})")
     send("Successful storage event")
 }
 
 fun main() {
     val logger = Logger("main")
-    val notificationSender = NotificationSender()
+    val notificationSender = NotificationSender("main")
     with(logger) {
         with(notificationSender) {
             store("an image")
